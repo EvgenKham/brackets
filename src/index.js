0724ce4e7fc8be1fdbdffa7f39f54879bdab69c9
EvgenKh <hamitcevich@gmail.com>
2024-09-02 16:03:07 +0300

    feat: finish brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig;
+  let stack = [];
+  let key = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if ( stack.length === 0 ){
+      stack.push(char);
+      continue;
+    }
+
+    brackets.forEach( item => {
+      if ( stack.slice(-1)[0] === item[0] ) {
+        key = item;
+      }
+    });
+
+    if ( char === key[1]) {
+      stack.pop();
+    } else if ( char !== key[1]){
+      stack.push(char);
+    }
+  }
+
+  if (stack.length !== 0) {
+    return false;
+  }
+  return true;
 }
